9c43840f820c57999c16e147784c7a3b27168688
EliasAlesand <elias.alesand@gmail.com>
2014-12-16 19:56:59 +0100

    Revert "Revert "Main structure of the project""
    This reverts commit 2b36a0bec63404a3310c26245dc46a908822d561.

---
+++ Party.java
@@ -0,0 +1,45 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Party {
+    private String partyName;
+    private int maxGuests;
+    private Money entranceFee;
+    private Map<Person, Boolean> guests;
+    private Map<Sponsor, Money> sponsors;
+
+    Party(String partyName, int maxGuests, Money entranceFee) {
+    this.partyName = partyName;
+    this.maxGuests = maxGuests;
+    this.entranceFee = entranceFee;
+    this.guests = new HashMap<Person, Boolean>();
+    this.sponsors = new HashMap<Sponsor, Money>();
+    }
+    
+    void addGuest(Person guest) {
+      addGuest(guest, false);
+    }
+
+    void addGuest(Person guest, boolean hasPaid) {
+        guests.put(guest, hasPaid);
+    }
+
+    void pay(Person guest) {
+        // Din kod här
+    }
+
+    boolean hasPaid(Person guest) throws NoSuchPersonException {
+        if(guests.containsKey(guest)) {
+            return guests.get(guest);
+        } else {
+            throw new NoSuchPersonException();
+        }
+    }
+
+    void addSponsor(Sponsor sponsor, Money amount) {
+        sponsors.put(sponsor, amount);
+    }
+    public void print() {
+        // Din kod här
+    }
+}
---
+++ Sponsor.java
@@ -0,0 +1,17 @@
+class Sponsor {
+    private final String name;
+
+    Sponsor(String name) {
+        this.name = name;
+    }
+
+    @Override
+        public String toString() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+}
