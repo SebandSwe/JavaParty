0a59fa85104e990e7cc51e789910195c6c47a432
EliasAlesand <elias.alesand@gmail.com>
2014-12-16 20:12:40 +0100

    GuestToStudent

--- Student.java
+++ Student.java
@@ -1,5 +1,14 @@
-class Student extends Person {
-    Student(String name) {
-        super(name);
-    }
+class Student extends Person{
+	Person guest;
+	Student(String name){
+		super(name);
+	}
+	Student(String name, GuestToStudent guest){
+		super(name);
+		this.guest = guest;
+		guest.setHost(this);
+	}
+	public GuestToStudent getGuest(){
+		return guest;
+	}
 }
